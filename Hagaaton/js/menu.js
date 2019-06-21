06a92b7ef5381aefd8581decdef14a77589fb95d
liatdavid2 <liatdavid2@gmail.com>
2019-06-21 13:19:59 +0300

    added 3 websites

---
+++ Hagaaton/js/menu.js
@@ -0,0 +1,21 @@
+/*  JavaScript Document                      */
+/*  Written by Chris Converse for lynda.com  */
+
+	$(document).ready(function(){
+		
+		
+	$('#nav.dropdown').css('display','block');
+	
+	$('#nav > ul').dropmenu({
+		effect : 'slide',
+		speed : 250,
+		timeout : 0,
+		nbsp : false
+				
+		});		
+		
+	});
+	
+	
+	
+	
